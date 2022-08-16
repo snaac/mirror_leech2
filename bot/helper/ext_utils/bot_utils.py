d9d30aab93bd6a93cbc26abdaa11969eb6da037a
Sa Sajjad <109235559+snaac@users.noreply.github.com>
2022-08-17 00:20:05 +0600

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -147,7 +147,7 @@ def get_readable_message():
             uname = f'<a href="tg://user?id={download.message.from_user.id}">{download.message.from_user.first_name}</a>'
             msg += f"╠➜<i>To Stop: </i><code>/{BotCommands.CancelMirror} {download.gid()}</code>\n"
             msg += f"╠═════════════════════════════╗\n"
-            msg += f'╠➜<i>Request By:</i> ️{uname}' \n
+            msg += f'╠➜<i>Request By:</i> ️{uname} \n'
             msg += f"╚═════════════════════════════╝\n"
             msg += f'\n<b>Source Msg: </b><a href="https://t.me/c/1711392078/{download.message.message_id}">Click Here</a>'
             msg += " "
